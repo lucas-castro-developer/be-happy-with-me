3af7a993689ec6de617bddb43353861cceac0c81
Lucas Castro <lucas.sc98@hotmail.com>
2021-04-30 20:38:36 -0300

    Building Header component

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 import './img/favicon.ico'
 import './css/index.css';
 import './css/pure.css';
+import App from './components/App'
 
 ReactDOM.render(
-    <h1>Bem-vindo ao React!</h1>,
+    <App />,
     document.querySelector("#main")
 )
