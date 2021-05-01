150ab2e154408b38d46168a75e95dc071b7a57ed
Lucas Castro <lucas.sc98@hotmail.com>
2021-04-30 21:29:11 -0300

    Adjusting Header style

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { ReactFragment } from "react";
 import Header from "./Header";
 import NovoUsuario from "./NovoUsuario";
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <div>
         <Header />
         <NovoUsuario />
-      </>
+      </div>
     );
   }
 }
