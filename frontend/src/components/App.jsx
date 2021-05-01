eb47fc139b1f77d9b8d3c4c412182121a940fb58
Lucas Castro <lucas.sc98@hotmail.com>
2021-04-30 21:15:52 -0300

    Adjusting App component

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
+import NovoUsuario from "./NovoUsuario";
 
 class App extends React.Component {
-    render() {
-        return (
-            <h1>Componente App!</h1>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Header />
+        <NovoUsuario />
+      </>
+    );
+  }
 }
 
 export default App;
