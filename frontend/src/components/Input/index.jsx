e7cedce5c484346a10df9b58e62665d6ee0e94d5
Lucas Castro <lucas.sc98@hotmail.com>
2021-05-09 20:27:17 -0300

    Input version two

--- frontend/src/components/Input/index.jsx
+++ frontend/src/components/Input/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Input(props) {
-  return <input type="text" {...props} />;
+  const estilo = {
+    borderColor: props.valorInvalido ? "#d50000" : "#cccccc",
+    backgroundColor: props.valorInvalido ? "ffcdd2" : "#ffffff",
+  };
+
+  return <input type="text" {...props} style={estilo} />;
 }
