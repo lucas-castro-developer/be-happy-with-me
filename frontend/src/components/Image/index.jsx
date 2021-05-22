84be2c0c1f030db26ee83c1d12b11c884b851cf0
Lucas Castro <lucas.sc98@hotmail.com>
2021-05-22 12:07:16 -0300

    Creating ButtonImage component

--- frontend/src/components/Image/index.jsx
+++ frontend/src/components/Image/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import './img/avatars.png'
+import "./img/avatars.png";
+import "./img/botoes.png";
 
 class Image extends React.Component {
   constructor(props) {
@@ -7,34 +8,33 @@ class Image extends React.Component {
   }
 
   calcularPosicaoX() {
-    return `${this.props.eixoX * this.props.width * (-1)}px`
+    return `${this.props.eixoX * this.props.width * -1}px`;
   }
-  
+
   calcularPosicaoY() {
-    return `${this.props.eixoY * this.props.height * (-1)}px`
+    return `${this.props.eixoY * this.props.height * -1}px`;
   }
 
   calcularTamanho() {
-      return `auto ${this.props.backgroundHeight}px`
+    return `auto ${this.props.backgroundHeight}px`;
   }
 
   obterEstilo() {
-      return {
-          backgroundImage: `url(${this.props.arquivo})`,
-          backgroundPositionX: this.calcularPosicaoX(),
-          backgroundPositionY: this.calcularPosicaoY(),
-          backgroundSize: this.calcularTamanho(),
-          width: `${this.props.width}px`,
-          height: `${this.props.height}px`,
-          display: 'table',
-          margin: '0 auto'
-      }
+    return {
+      backgroundImage: `url(${this.props.arquivo})`,
+      backgroundPositionX: this.calcularPosicaoX(),
+      backgroundPositionY: this.calcularPosicaoY(),
+      backgroundSize: this.calcularTamanho(),
+      width: `${this.props.width}px`,
+      height: `${this.props.height}px`,
+      display: "table",
+      margin: "0 auto",
+    };
   }
 
   render() {
-      return <div style={this.obterEstilo()}></div>
+    return <div style={this.obterEstilo()}></div>;
   }
-
 }
 
 export default Image;
