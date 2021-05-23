2f2368d6bb03020e555a6d9e16f97140619bbc39
Lucas Castro <lucas.sc98@hotmail.com>
2021-05-23 19:05:19 -0300

    Creating new render methods

--- frontend/src/components/ImageScroller/index.jsx
+++ frontend/src/components/ImageScroller/index.jsx
@@ -18,6 +18,55 @@ class ImageScroller extends React.Component {
   obterSelecionado() {
     return this.props.elementos[this.state.manipularEvento.index];
   }
+
+  renderizarImagem(entry, index) {
+    let eixoY = this.props.eixoY ? this.props.eixoY : 0;
+    return (
+      <li
+        style={{
+          paddingTop: "8px",
+          position: "absolute",
+          zIndex: "-1",
+          marginLeft: `${index * 140}px`,
+        }}
+        key={index + entry.toString()}
+      >
+        <Image
+          eixoX={entry.index}
+          eixoY={eixoY}
+          width={140}
+          height={140}
+          backgroundHeight={280}
+          arquivo={this.props.arquivo}
+        />
+      </li>
+    );
+  }
+
+  renderizarImagens() {
+    const ms = this.state.manipularEvento.toqueEmExecucao ? "100ms" : "800ms";
+
+    const estilo = {
+      WebkitTransitionDuration: ms,
+      MsTransitionDuration: ms,
+      MozTransitionDuration: ms,
+      OTransitionDuration: ms,
+      transitionDuration: ms,
+
+      listStyleType: "none",
+      margin: "0",
+      padding: "0",
+      position: "relative",
+      width: `${this.props.elementos.length * 140}px`,
+      left: `${this.state.manipularEvento.left}px`,
+    };
+
+    const lista = this.props.elementos.map((entry, index) =>
+      this.renderizarImagem(entry, index)
+    );
+
+    return <li style={estilo}>{lista}</li>;
+  }
 }
 
 export default ImageScroller;
