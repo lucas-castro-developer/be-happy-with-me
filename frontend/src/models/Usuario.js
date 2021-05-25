4af97b5ebb24af6605e1f4ac19d8028852d10416
Lucas Castro <lucas.sc98@hotmail.com>
2021-05-25 19:03:19 -0300

    Adjust User model

--- frontend/src/models/Usuario.js
+++ frontend/src/models/Usuario.js
@@ -1,10 +1,30 @@
 import Avatar from "./Avatar";
+import Repository from "../infrastructure/Repository";
+const repository = new Repository();
 
 class Usuario {
   constructor() {
     this.nome = "";
     this.genero = "";
     this.avatar = Avatar.obterTodos()[0];
+  }
+
+  salvar(callback) {
+    repository.salvar(this, callback);
+  }
+
+  static obter(sucesso, falha) {
+    repository.obter((json) => {
+      let usuario = new Usuario();
+      usuario.nome = json.nome;
+      usuario.genero = json.genero;
+      usuario.avatar = new Avatar(json.avatar.index, json.avatar.descricao);
+      sucesso(usuario);
+    }, falha);
+  }
+
+  toString() {
+    return `${this.nome}, ${this.avatar.toString()}`;
   }
 
   validarNome() {
